3182da8fec833e70b895fd1a79d1a1d70714a8f3
Jose Arroyo <jc.arroyo10@gmail.com>
2016-02-14 11:43:20 -0500

    Modificación a vistas de jade

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 var express = require('express'),
 	helmet = require('helmet'),
-	app = express();
+	app = express(),
+	path = require('path');
 
-app.use(express.static(__dirname + '/web'));
+
+app.use(helmet());
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'jade');
+
+var ms_hour = 1000 * 60 * 60;
+app.use(express.static(path.join(__dirname, '/public'), {maxAge: ms_hour * 12}));
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/nosotros', function(req, res){
+	res.render('about');
+});
+
+
+
+app.use(function(req, res){
+	res.render('404');
+});
 
 app.listen(80);
